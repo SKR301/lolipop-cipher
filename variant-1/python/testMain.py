3c6a18ba876e8684490b4feaa671fb1a80b4d060
SKR301 <chunnu.bihat@gmail.com>
2022-06-25 00:43:42 +0530

    added test structure for encrypt

--- variant-1/python/testMain.py
+++ variant-1/python/testMain.py
@@ -1,2 +1,52 @@
 import unittest
 from Lolipop import Lolipop
+
+class TestLolipop(unittest.TestCase):
+    def test_validatePadInput(self):
+        # size<36
+        # size = 36 but value replaced 
+        # no input 
+        # default input passed 
+        # correct input 
+        print()
+
+    def test_createPadMatrix(self):
+        # default pad matrix
+        # manual input pad string 
+        print()
+
+    def test_shiftRight(self):
+        # row < 0
+        # row > 6
+        # shift count < 0
+        # correct amount 
+        print()
+
+    def test_shiftDown(self):
+        # col < 0
+        # col > 6
+        # shift count < 0
+        # correct amount 
+        print()
+
+    def test_shiftLeft(self):
+        # row < 0
+        # row > 6
+        # shift count < 0
+        # correct amount 
+        print()
+
+    def test_shiftUp(self):
+        # col < 0
+        # col > 6
+        # shift count < 0
+        # correct amount 
+        print()
+
+    def test_encrypt(self):
+        # empty string
+        # size = 1
+        # size > 36
+        # same character repeated 10 times 
+        # same pair repeated 10 times 
+        print()
