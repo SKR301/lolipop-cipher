b8bb332fc7b68a6372ab8222e851a0adebb5e0a1
SKR301 <chunnu.bihat@gmail.com>
2022-06-28 22:31:56 +0530

    change file name

--- src/python/main.py
+++ src/python/main.py
@@ -3,20 +3,18 @@ from Lolipop import PadMatrix
 
 if __name__ == '__main__':
     plainText = 'HELLO'
+    
     cipher = Lolipop()
     crypt = cipher.encrypt(plainText)
-
     cipherText = crypt['cipherText']
     key = crypt['key']
-    print()
-    print('PlainText:', plainText)
-    print('Crypted:', cipherText)
-    print('Key: ', key)
-    #     PadMatrix().printPadMatrix(cipher.createPadMatrix(key))
-    #     print()
 
     cipher = Lolipop(key)
     decrypt = cipher.decrypt(cipherText)
+
+    print('PlainText:', plainText)
+    print('Crypted:', cipherText)
+    print('Key: ', key)
     print('Decrypted:', decrypt)
 
         
